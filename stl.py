76e2e3349ca2c2d00da49ca0a777576c299fe153
b-tribe-ru <167023018+b-tribe-ru@users.noreply.github.com>
2024-04-20 13:12:29 +0300

    Update stl.py

--- stl.py
+++ stl.py
@@ -26,7 +26,7 @@ st.image(image, caption=None, use_column_width=None, clamp=False, channels="RGB"
 # Create a connection object.
 conn = st.connection("gsheets", type=GSheetsConnection)
 df = conn.read()
-
+st.write(st.secrets['connections'])
 # Print results.
 for row in df.itertuples():
     st.write(f"{row.expert} has a :{row.project}:")
